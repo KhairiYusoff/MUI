e3326450a03ab5fbc6ebd217d608c6cb9cfabb2f
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2024-01-01 03:10:54 +0800

    Created ColorButtons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ComboBox from "./components/Inputs/Autocomplete/ComboBox"
 import Playground from "./components/Inputs/Autocomplete/Playground"
 import BasicButtons from "./components/Inputs/Button/Basic"
+import ColorButtons from "./components/Inputs/Button/Color"
 import OutlinedButtons from "./components/Inputs/Button/Outlined"
 import TextButtons from "./components/Inputs/Button/Text"
 import Checkboxes from "./components/Inputs/Checkbox/Basic"
@@ -11,7 +12,7 @@ function App() {
 
   return (
     <>
-      <OutlinedButtons />
+      <ColorButtons />
     </>
   )
 }
