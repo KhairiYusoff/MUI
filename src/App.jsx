3e76474c1871e94237c0c4270ec904b18bc4ae23
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2024-01-01 03:09:15 +0800

    Created OutlinedButtons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ComboBox from "./components/Inputs/Autocomplete/ComboBox"
 import Playground from "./components/Inputs/Autocomplete/Playground"
 import BasicButtons from "./components/Inputs/Button/Basic"
+import OutlinedButtons from "./components/Inputs/Button/Outlined"
 import TextButtons from "./components/Inputs/Button/Text"
 import Checkboxes from "./components/Inputs/Checkbox/Basic"
 
@@ -10,7 +11,7 @@ function App() {
 
   return (
     <>
-      <TextButtons />
+      <OutlinedButtons />
     </>
   )
 }
