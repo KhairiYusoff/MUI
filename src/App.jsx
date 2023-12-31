3a34538c1ce92d72b66b79f5e495dfdab4424972
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2024-01-01 03:08:25 +0800

    Created TextButtons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ComboBox from "./components/Inputs/Autocomplete/ComboBox"
 import Playground from "./components/Inputs/Autocomplete/Playground"
 import BasicButtons from "./components/Inputs/Button/Basic"
+import TextButtons from "./components/Inputs/Button/Text"
 import Checkboxes from "./components/Inputs/Checkbox/Basic"
 
 
@@ -9,7 +10,7 @@ function App() {
 
   return (
     <>
-      <BasicButtons />
+      <TextButtons />
     </>
   )
 }
