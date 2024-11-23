51658795901b8143af3d2ca0cba23710af60a3dd
Mohd Khairi Mohd Yusoff <mohdkhairi.mohdyusoff@maybank.com>
2024-11-23 16:12:56 +0800

    v5 - Basic pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
-import ComboBox from "./components/Inputs/Autocomplete/ComboBox"
-import Playground from "./components/Inputs/Autocomplete/Playground"
-import BasicButtons from "./components/Inputs/Button/Basic"
-import ColorButtons from "./components/Inputs/Button/Color"
-import OutlinedButtons from "./components/Inputs/Button/Outlined"
-import TextButtons from "./components/Inputs/Button/Text"
-import Checkboxes from "./components/Inputs/Checkbox/Basic"
-import BoxBasic from "./components/Layouts/Box/Basic"
-import BoxSystemProps from "./components/Layouts/Box/Custom1"
-import SimpleBottomNavigation from "./components/Navigation/BottomNav/BottomNav"
-import LabelBottomNavigation from "./components/Navigation/BottomNav/BottomNav2"
-import FixedBottomNavigation from "./components/Navigation/BottomNav/BottomNav3"
-import Tabs1 from "./components/Navigation/Tabs/Tabs1"
-import Tabs2 from "./components/Navigation/Tabs/Tabs2"
-import Tabs3 from "./components/Navigation/Tabs/Tabs3"
-import Tabs4 from "./components/Navigation/Tabs/Tabs4"
-import Breadcrumbs1 from "./components/Navigation/Breadcrumbs/Breadcrumbs1"
-import LandingPage from "./components/Landing Page/LandingPage"
-
+import ComboBox from "./components/Inputs/Autocomplete/ComboBox";
+import Playground from "./components/Inputs/Autocomplete/Playground";
+import BasicButtons from "./components/Inputs/Button/Basic";
+import ColorButtons from "./components/Inputs/Button/Color";
+import OutlinedButtons from "./components/Inputs/Button/Outlined";
+import TextButtons from "./components/Inputs/Button/Text";
+import Checkboxes from "./components/Inputs/Checkbox/Basic";
+import BoxBasic from "./components/Layouts/Box/Basic";
+import BoxSystemProps from "./components/Layouts/Box/Custom1";
+import SimpleBottomNavigation from "./components/Navigation/BottomNav/BottomNav";
+import LabelBottomNavigation from "./components/Navigation/BottomNav/BottomNav2";
+import FixedBottomNavigation from "./components/Navigation/BottomNav/BottomNav3";
+import Tabs1 from "./components/Navigation/Tabs/Tabs1";
+import Tabs2 from "./components/Navigation/Tabs/Tabs2";
+import Tabs3 from "./components/Navigation/Tabs/Tabs3";
+import Tabs4 from "./components/Navigation/Tabs/Tabs4";
+import Breadcrumbs1 from "./components/Navigation/Breadcrumbs/Breadcrumbs1";
+import LandingPage from "./components/Landing Page/LandingPage";
+import Pagination1 from "./components/Navigation/Pagination/Pagination1";
 
 function App() {
-
   return (
     <>
-      <LandingPage/>
+      <Pagination1 />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
